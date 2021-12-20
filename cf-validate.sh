0741432950e05dfab712dffec8dd70736978efc7
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-12-20 16:08:49 -0500

    add certificate creation

--- cf-validate.sh
+++ cf-validate.sh
@@ -1,3 +1,4 @@
 echo "Starting validation..."
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev.template --profile personal
+aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-acm.template --profile personal
 echo "Validation complete."
