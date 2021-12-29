5dc09d6488b9b51c11cc7fef90620c67cdb24249
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2021-12-29 00:54:41 -0500

    add route53 support

--- cf-validate.sh
+++ cf-validate.sh
@@ -1,4 +1,10 @@
 echo "Starting validation..."
+echo "--Main Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev.template --profile personal
+echo "--Certificate Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-acm.template --profile personal
+echo "KSK Template"
+aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-dns-ksk.template --profile personal
+echo "--DNS Template--"
+aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-dns.template --profile personal
 echo "Validation complete."
