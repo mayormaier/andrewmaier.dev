5a9500d9c5c1bbeb0ad1baa2e901d5774a0309bb
Andrew Maier <70287117+mayormaier@users.noreply.github.com>
2022-01-03 00:30:59 -0500

    add proper indexing with lambda at edge

--- cf-validate.sh
+++ cf-validate.sh
@@ -3,8 +3,10 @@ echo "--Main Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev.template --profile personal
 echo "--Certificate Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-acm.template --profile personal
-echo "KSK Template"
+echo "--KSK Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-dns-ksk.template --profile personal
 echo "--DNS Template--"
 aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-dns.template --profile personal
+echo "--Lambda@Edge Template--"
+aws cloudformation validate-template --template-body file://./.aws/andrewmaier.dev-lambda.template --profile personal
 echo "Validation complete."
